cc52de66d195c39d74383e3fbb0838e168243511
Rafid Dewan <32348632+rafiddewan@users.noreply.github.com>
2019-09-23 12:56:19 -0400

    removed line

--- src/BuddyInfo.java
+++ src/BuddyInfo.java
@@ -1,50 +1,58 @@
 
-public class BuddyInfo {
+public class BuddyInfo 
+{
 
 	private String name, phoneNum, address; 
 	
-	public BuddyInfo(String name, String phoneNum, String address) {
+	public BuddyInfo(String name, String phoneNum, String address) 
+	{
 		this.name = name;
 		this.phoneNum = phoneNum;
 		this.address = address;
 	}
 
 
-	public String getName() {
+	public String getName() 
+	{
 		return name;
 	}
 
 
-	public void setName(String name) {
+	public void setName(String name) 
+	{
 		this.name = name;
 	}
 
 
-	public String getPhoneNum() {
+	public String getPhoneNum() 
+	{
 		return phoneNum;
 	}
 
 
-	public void setPhoneNum(String phoneNum) {
+	public void setPhoneNum(String phoneNum) 
+	{
 		this.phoneNum = phoneNum;
 	}
 
 
-	public String getAddress() {
+	public String getAddress() 
+	{
 		return address;
 	}
 
 
-	public void setAddress(String address) {
+	public void setAddress(String address) 
+	{
 		this.address = address;
 	}
 	
-	public boolean equals(Object obj) {
+	public boolean equals(Object obj) 
+	{
 		if (obj== this) return true;
 		if (obj== null) return false;
 		if (this.getClass() != obj.getClass()) return false;	
 		BuddyInfo buddy = (BuddyInfo) obj;
 		return buddy.getName() == this.getName() && buddy.getPhoneNum() == this.getPhoneNum() && buddy.getAddress() == this.getAddress();
 	}
-
 }
