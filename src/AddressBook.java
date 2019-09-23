c9558d4eb4268fa48f02c883c089740facf66892
rafiddewan <rafiddewan@cb5109-34.labs.sce.carleton.ca>
2019-09-23 12:17:40 -0400

    Making a BUddyInfo and AddressBOok class to test Eclipse git integration

---
+++ src/AddressBook.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+
+public class AddressBook 
+{
+
+	private ArrayList<BuddyInfo> buddys;
+
+	public AddressBook() 
+	{
+		this.buddys = new ArrayList<BuddyInfo>();
+	}
+	
+	public boolean addBuddy(BuddyInfo thisBuddy) 
+	{
+		if(thisBuddy != null) {
+			buddys.add(thisBuddy);
+		}
+		return false;
+	}
+	
+	public boolean removeBuddy(int index) 
+	{
+		if(index >= 0 && index < buddys.size())
+		{
+			buddys.remove(index);
+			return true;
+		}
+		return false;
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", "1234");
+		AddressBook addressBook =  new AddressBook();
+		addressBook.addBuddy(buddy);
+		addressBook.removeBuddy(0);
+	}
+}
---
+++ src/BuddyInfo.java
@@ -0,0 +1,50 @@
+
+public class BuddyInfo {
+
+	private String name, phoneNum, address; 
+	
+	public BuddyInfo(String name, String phoneNum, String address) {
+		this.name = name;
+		this.phoneNum = phoneNum;
+		this.address = address;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getPhoneNum() {
+		return phoneNum;
+	}
+
+
+	public void setPhoneNum(String phoneNum) {
+		this.phoneNum = phoneNum;
+	}
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public boolean equals(Object obj) {
+		if (obj== this) return true;
+		if (obj== null) return false;
+		if (this.getClass() != obj.getClass()) return false;	
+		BuddyInfo buddy = (BuddyInfo) obj;
+		return buddy.getName() == this.getName() && buddy.getPhoneNum() == this.getPhoneNum() && buddy.getAddress() == this.getAddress();
+	}
+
+}
